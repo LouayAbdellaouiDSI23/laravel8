0041bce1382924da564d13aff87a2d3d0802284d
louay abdellaoui <louay.abdellaoui217@gmail.com>
2022-05-26 15:47:02 +0100

    mon second commit

---
+++ atelier1/app/Models/Scategorie.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Scategorie extends Model
+{
+use HasFactory;
+protected $fillable = [
+'nomscategorie','imagescat','categorieID'
+];
+public function scategories()
+{
+return $this->hasMany(Scategorie::class ,"categorieID");
+}
+}
---
+++ atelier1/resources/js/components/Categories/AddCategorie.vue
@@ -0,0 +1,46 @@
+<template lang="">
+
+<form @submit.prevent="ajouterCategorie">
+ <div class="form-group">
+ <input type="text" class="form-control" placeholder="Nom" vmodel="nomcategorie">
+ </div>
+ <div class="form-group">
+ <input type="file" class="form-control"
+placeholder="Image" @change="onFileChange">
+</div>
+
+ <button type="submit" class="btn btn-block btn-primary">Ajouter
+Catégorie</button>
+</form>
+</template>
+<script>
+export default {
+ data() {
+ return {
+ nomcategorie: "",
+ imagecategorie: "",
+ }
+ },
+ methods: {
+
+ ajouterCategorie(){
+ const categ = {
+ nomcategorie:this.nomcategorie,
+ imagecategorie:this.imagecategorie
+ }
+ this.axios.post("http://localhost:8000/api/categories",categ)
+ .then(() => {
+ this.$router.push('/categories')})
+ .catch(error => {
+ this.errorMessage = error.message;
+ console.error("There was an error!", error);})
+ },
+ onFileChange(e) {
+ this.imagecategorie = "categories/"+e.target.files[0].name;
+ }
+ }
+}
+</script>
+<style lang="">
+
+</style>
